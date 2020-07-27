c9f0f0e9736ce74d1ee479e97b3685e897451f32
xiaojianhan19 <xiao.tijiao@neso.co.jp>
2020-07-28 04:52:22 +0900

    data

---
+++ data/sql/sequence_noneed.sql
@@ -0,0 +1,7 @@
+alter sequence event_id_seq restart with 10000764;
+alter sequence event_item_id_seq restart with 11006153;
+alter sequence collection_id_seq restart with 30000162;
+alter sequence collection_item_id_seq restart with 31000016;
+alter sequence person_id_seq restart with 21000065;
+alter sequence category_id_seq restart with 40000013;
+alter sequence category_item_id_seq restart with 41000266;
